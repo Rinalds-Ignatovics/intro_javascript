224ba28b60add751cbcc340774fb46a398ec8af5
DEYZEN <rinalds.ignatovicsdp13@inbox.lv>
2021-03-02 14:15:34 +0200

    pievienoju js

---
+++ static/mans_scripts.js
@@ -0,0 +1,9 @@
+console.log("Viss strādā!")
+//alert("Hello!")
+alert("Rinalds")
+
+let mainigaisTeksts = "skola"
+let mainigaisSkaitlis = 7
+
+console.log(mainigaisTeksts)
+console.log(mainigaisSkaitlis)
